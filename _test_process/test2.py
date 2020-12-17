064c78452df06ecfb1a4ed91247ed9d84e8423e7
Krzyzaku21 <krzyzak21@gmail.com>
2020-12-17 21:43:33 +0100

    test

--- _test_process/test2.py
+++ _test_process/test2.py
@@ -2,3 +2,48 @@ d = {'spam': 1, 'eggs': 2, 'cheese': 3}
 e = {'cheese': 'cheddar', 'aardvark': 'Ethel'}
 f = e | d
 print(f)
+
+def pivot():
+    Flexmonster = {
+        'container' : 'pivot',
+        'componentFolder': "https://cdn.flexmonster.com/",
+        'height': 600,
+        'toolbar': True,
+        'report': {
+            "dataSource": {
+                "type": "api",
+                "url": "http://localhost:9204/mongo", # the url where our connector is running
+                "index": "marketing", # specify the collection’s name
+            },
+            "slice": {
+                "rows": [
+                {
+                    "uniqueName": "Country",
+                }
+            ],
+            "columns": [
+                {
+                    "uniqueName": "[Measures]",
+                }
+            ],
+            "measures": [
+                {
+                    "uniqueName": "Leads",
+                    "aggregation": "sum",
+                },
+                {
+                    "uniqueName": "Opportunities",
+                    "aggregation": "sum",
+                }
+            ],
+            }
+        }
+    }
+def pivot_charts():
+    Flexmonster = {
+        'container' : 'pivot_charts',
+        'componentFolder': "https://cdn.flexmonster.com/",
+        'height': 600,
+        'toolbar': True,
+        'report': {}
+    }
